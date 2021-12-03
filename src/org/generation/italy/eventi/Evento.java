172d8767ebdb22a5a016bf83520e9aee7abb7d93
luisabattajni <luisasara.battajni@gmail.com>
2021-12-03 15:30:26 +0100

    slow wip 3

--- src/org/generation/italy/eventi/Evento.java
+++ src/org/generation/italy/eventi/Evento.java
@@ -5,8 +5,6 @@ import java.time.format.DateTimeFormatter;
 // formattazione per dopo
 public class Evento {
 	
-
-	//@prof se dimentico di cancellare i commenti per l'ultimo push prego ignorare 
 	
 	
 	// attributi
@@ -72,9 +70,9 @@ public class Evento {
 	
 
 	//prova? da controllare
-	public int prenota () throws Exception {
-		if (data.isAfter(LocalDate.now()) && postiPrenotati <= postiTot) {
-			return postiPrenotati + 1;
+	public int prenota() throws Exception {
+		if (data.isAfter(LocalDate.now()) && postiPrenotati < postiTot) {
+			return ++postiPrenotati;
 		} else if (postiPrenotati > postiTot) {
 			throw new Exception("Non ci sono così tanti posti disponibili.");
 		} else {
@@ -82,19 +80,41 @@ public class Evento {
 		}
 
 	}
+	public int prenota(int n) throws Exception {
+		if (data.isAfter(LocalDate.now()) && postiPrenotati < postiTot) {
+				return postiPrenotati + n;
+			} else if (postiPrenotati > postiTot) {
+			throw new Exception("Non ci sono così tanti posti disponibili.");
+		} else {
+			throw new Exception("Questa data è già passata.");
+		}
+	}
+	
 
 
-	public int disdici () throws Exception {
+	public int disdici() throws Exception {
 		if (data.isAfter(LocalDate.now()) && postiPrenotati == 0) {
-			return postiPrenotati - 1;
+			return --postiPrenotati;
 		} else if (postiPrenotati == 0) {
 			throw new Exception("Non ci sono prenotazioni.");
 		} else {
 			throw new Exception("Questa data è già passata.");
 		}
-
-		
+	
 	}
+	
+	public int disdici(int n) throws Exception {
+		if (data.isAfter(LocalDate.now()) && postiPrenotati == 0) {
+			return postiPrenotati - n;
+		} else if (postiPrenotati == 0) {
+			throw new Exception("Non ci sono prenotazioni.");
+		} else {
+			throw new Exception("Questa data è già passata.");
+		}
+	
+	}
+
+
 
 	
 	//override data formattata - titolo
--- src/org/generation/italy/eventi/Main.java
+++ src/org/generation/italy/eventi/Main.java
@@ -9,18 +9,19 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
+		
+		int postiPrenotati = 0;
 
-		//lato organizzativo
-		
-		
+		// lato organizzativo
+
 		System.out.println("Inserite il titolo dell'evento che desiderate organizzare: ");
 		String titolo = scan.nextLine();
-		//sposto un attimo sopra i posti, finché non risolvo data
+		// sposto un attimo sopra i posti, finché non risolvo data
 		System.out.println("Inserite il numero totale dei posti: ");
 		int postiTot = scan.nextInt();
-		
-		//LocalDate???
-		
+
+		// LocalDate???
+
 //		System.out.println("Inserite la data (dd/MM/yyyy): ");
 		System.out.println("giorno: ");
 		int dd = scan.nextInt();
@@ -30,45 +31,68 @@ public class Main {
 		int yyyy = scan.nextInt();
 		LocalDate data = LocalDate.of(yyyy, mm, dd);
 //		System.out.println("Data: " + data);
-		//debug
-		
-		
+		// debug
 
-		//lato utente
-		
+		// lato utente
+		scan.nextLine();
+
 		try {
-		Evento evento = new Evento (titolo, data, postiTot);
+			Evento evento = new Evento(titolo, data, postiTot);
 
-		boolean vuoiPrenotare = true;
-		boolean errore = false;
-		
-        System.out.println("Si desidera effettuare una prenotazione? s/n: ");
-        String prenotaSN = scan.nextLine();
+//		boolean vuoiPrenotare = true;
+//		boolean errore = false;
 
-		
-		if (prenotaSN.equals("s")) {
-			//continua chiedendo quanti posti
-			System.out.println("Prova");
-		} else if (prenotaSN.equals("n")) {
-			System.out.println("Operazione terminata"); //bit ominous
-		} else {
-          System.out.println("Prego utilizzare unicamente (s/n).");
+			System.out.println("Si desidera effettuare una prenotazione? s/n: ");
+			String continua = scan.nextLine();
 
-		}
-		
+			if (continua.equals("s")) {
+				// continua chiedendo quanti posti
+				System.out.println("Quanti posti si desidera prenotare? ");
+				int postiAgg = scan.nextInt();
+				System.out.println(evento.prenota(postiAgg));
+				postiPrenotati = evento.prenota(postiAgg);
 
-			
+			} else if (continua.equals("n")) {
+				System.out.println("Operazione terminata");
+			} else {
+				System.out.println("Prego utilizzare unicamente (s/n)");
+			}
 
-			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
 
+		System.out.println("Numero di posti prenotati: " + postiPrenotati);
+		//non mi parte l'errore che dovrebbe dare se chiedo più posti del tot
+		int postiDisponili = postiTot - postiPrenotati;
+		System.out.println("Numero di posti disponibili: " + postiDisponili);
+		
+		
+		scan.nextLine();
+//		// disdette
+		try {
+			Evento evento = new Evento(titolo, data, postiTot);
+			System.out.println("Si desidera disdire prenotazione? s/n: ");
+			String cancel = scan.nextLine();
 
-		
-		
-scan.close();
+			if (cancel.equals("s")) {
+				System.out.println("Quanti posti si desidera disdire? ");
+				int disd = scan.nextInt();
+				System.out.println(evento.disdici(disd));
+			} else if (cancel.equals("n")) {
+				System.out.println("Operazione terminata");
+			} else {
+				System.out.println("Prego utilizzare unicamente (s/n)");
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+//		System.out.println("Numero di posti prenotati: " + postiPrenotati);
+//		System.out.println("Numero di posti disponibili: " + postiDisponili);
+
+		scan.close();
 	}
-	
+
 }
